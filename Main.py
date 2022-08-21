4afa14d6ec93198c9691998d77ccb2eed7dc931f
AlamRidha <alamrridha@gmail.com>
2022-08-21 16:36:16 +0700

    membuat fungsi reamur

--- Main.py
+++ Main.py
@@ -14,3 +14,18 @@ def konversiCelcius(celcius):
     # kelvin
     kelvin = celcius + 273
     print(celcius, " Celcius = ", kelvin, " Kelvin")
+
+
+# fungsi konversi reamur ke celcius, fahreinhet dan kelvin
+def konversiReamur(reamur):
+    # celcius
+    celcius = (5/4) * reamur
+    print(reamur, " Reamur = ", celcius, " Celcius")
+
+    # fahrenheit
+    fahrenheit = ((9/4) * reamur) + 32
+    print(reamur, " Reamur = ", fahrenheit, " Fahrenheit")
+
+    # kelvin
+    kelvin = ((5/4) * reamur) + 273
+    print(reamur, " Reamur = ", kelvin, " Kelvin")
